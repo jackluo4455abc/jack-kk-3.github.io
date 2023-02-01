fad1465b03bb25b670d4b761717a72ad2452bf20
jackluo4455abc <124121921+jackluo4455abc@users.noreply.github.com>
2023-02-01 13:50:52 +0800

    計算機 版本1

---
+++ kk3/kk3.js
@@ -0,0 +1,10 @@
+//製作簡單版計算機 只限加減
+//kk.html引入程式
+//<script src="kk3.js"></script>
+//js檔案區
+var num1=prompt("請輸入第一個數");
+var num2=prompt("請輸入第二個數");
+num1=parseFloat(num1);
+num2=parseFloat(num2);
+document.write("運算結果=");
+document.write(num1+num2);
